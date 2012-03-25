ebb83a5731e17ed3ce95354fee51bbb0837d1a43
Ben Cochran <ben.m.cochran@gmail.com>
2012-03-24 20:34:34 -0500

    web clients push data back to the server

--- game.rb
+++ game.rb
@@ -4,6 +4,7 @@ require 'bundler/setup'
 require 'sqlite3'
 require 'sinatra/sequel'
 require 'sinatra'
+require 'juggernaut'
 set :database, 'sqlite://data.db'
 
 #require 'models/models.rb'
@@ -11,4 +12,12 @@ set :database, 'sqlite://data.db'
   
 get '/' do
   haml :loadin
+end
+
+post '/events/new' do
+  Juggernaut.publish("channel1", "new event: #{params[:will]} - #{params[:you]}")
+end
+
+post '/query' do
+  Juggernaut.publish("channel1", "query: #{params[:will]} - #{params[:you]}")
 end
--- public/js/jug.js
+++ public/js/jug.js
@@ -1,25 +1,47 @@
-  var logElement = jQuery("#log");
-  logElement.val("hello");
+var logElement = jQuery("#log");
+logElement.val("hello");
 
-  var log = function(data){
-    logElement.val( logElement.val() + data + "\n");
-    logElement.scrollTop(
-      logElement[0].scrollHeight - logElement.height()
-    );
-  };
+var log = function(data){
+  logElement.val( logElement.val() + data + "\n");
+  logElement.scrollTop(
+    logElement[0].scrollHeight - logElement.height() - 24
+  );
+};
 
-  var jug = new Juggernaut({
-    secure: ('https:' == document.location.protocol),
-    host: document.location.hostname,
-    port: 8080
+var jug = new Juggernaut({
+  secure: ('https:' == document.location.protocol),
+  host: document.location.hostname,
+  port: 8080
+});
+
+jug.on("connect", function(){ log("Connected") });
+jug.on("disconnect", function(){ log("Disconnected") });
+jug.on("reconnect", function(){ log("Reconnecting") });
+
+log("Subscribing to channel1");
+
+jug.subscribe("channel1", function(data){
+  log("Got data: " + data);
+});
+
+/* data handling */
+
+$(document).ready(function() {
+  $('#new_event').submit(function() {
+    $.post($(this).attr('action'), $(this).serialize(), function(data){
+      $('#message').append("<p>" + data + "</p>");
+      $('#new_message').each(function(){this.reset();});
+    }, "text");
+    return false;
   });
+});
 
-  jug.on("connect", function(){ log("Connected") });
-  jug.on("disconnect", function(){ log("Disconnected") });
-  jug.on("reconnect", function(){ log("Reconnecting") });
-
-  log("Subscribing to channel1");
-
-  jug.subscribe("channel1", function(data){
-    log("Got data: " + data);
+$(document).ready(function() {
+  $('#new_query').submit(function() {
+    $.post($(this).attr('action'), $(this).serialize(), function(data){
+      $('#message').append("<p>" + data + "</p>");
+      $('#new_message').each(function(){this.reset();});
+    }, "text");
+    return false;
   });
+});
