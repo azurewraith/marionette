0c177d31dcdbb5ccaf6df2524d1b7f9cf419b1de
Ben Cochran <ben.m.cochran@gmail.com>
2012-04-07 13:52:40 -0500

    Event class serialize/deserialize in Ruby and JS Initial Polygon implementation Actions for subscribe/unsubscribe

--- game.rb
+++ game.rb
@@ -4,7 +4,11 @@ require 'bundler/setup'
 require 'sqlite3'
 require 'sinatra/sequel'
 require 'sinatra'
+require 'haml'
 require 'juggernaut'
+require 'json'
+require 'json/add/core'
+
 set :database, 'sqlite://data.db'
 
 #require 'models/models.rb'
@@ -14,12 +18,173 @@ get '/' do
   haml :loadin
 end
 
+event_count = 0;
+
+# takes location, range, time, duration, type
 post '/events/new' do
-  Juggernaut.publish("channel1", "new event: #{params[:will]} - #{params[:you]}")
-  "hi there!"
+  location = params[:location].first.split('.').map{|i| i.to_i}
+  range    = params[:range].to_i
+  time     = params[:time]
+  duration = params[:duration].to_i
+  type     = params[:type]
+
+  # assign a loose numerical identifier to it
+  id = event_count
+  event_count = event_count + 1
+
+  e = Event.new(id, location, range, time, duration, type)
+
+  # determine what neighborhood location is in, publish to that neighborhood
+  zone = "blah"
+
+  Juggernaut.publish("global", e.to_json)
+  "Event #{id} was published to zone #{zone}"
 end
 
+# takes location, range, so_and_so
 post '/query' do
-  Juggernaut.publish("channel1", "query: #{params[:will]} - #{params[:you]}")
-  ""
+  location  = params[:location]
+  range     = params[:range]
+  so_and_so = "strongbad and spaceships"
+
+  # determine neighborhoods where range intersects from location
+  zones = []
+
+  Juggernaut.publish("global", "query: #{params[:will]} - #{params[:you]}")
+  "We asked #{zones} about #{so_and_so}"
 end
+
+# takes client_id, location
+post '/starships/new' do
+  captain  = params[:client_id]
+  location = params[:location]
+  s = Starship.new(captain, location, 5, 5, 20, 100)
+  s.to_json
+end
+
+Thread.new do
+  Juggernaut.subscribe do |event, data|
+    case event
+    when :subscribe
+      # new fish
+      # add client to neighborhood, calculate new zones, if area of new zone > threshold then push update
+      # if refreshing, :unsubscribe and :subscribe are nearly instantaneous, maybe buffer in a queue that waits?
+      puts "subscribing #{data.inspect}"
+    when :unsubscribe
+      # fish out of water
+      # remove client from neighborhood, if a super client disconnected recalculate zones
+      puts "unsubscribing #{data.inspect}"
+    else
+      puts "unknown event: #{event}"
+    end
+  end
+end
+
+# An Array of points
+# 
+# Most methods from:
+# http://jakescruggs.blogspot.com/2009/07/point-inside-polygon-in-ruby.html
+class Polygon < Array
+
+  def include?(point)
+    return false if outside_bounding_box?(point)
+    contains_point = false
+    i = -1
+    j = self.size - 1
+    while (i += 1) < self.size
+      a_point_on_polygon = self[i]
+      trailing_point_on_polygon = self[j]
+      if point_is_between_the_ys_of_the_line_segment?(point, a_point_on_polygon, trailing_point_on_polygon)
+        if ray_crosses_through_line_segment?(point, a_point_on_polygon, trailing_point_on_polygon)
+          contains_point = !contains_point
+        end
+      end
+      j = i
+    end
+    return contains_point
+  end
+
+private
+
+  def point_is_between_the_ys_of_the_line_segment?(point, a_point_on_polygon, trailing_point_on_polygon)
+    (a_point_on_polygon.y <= point.y && point.y < trailing_point_on_polygon.y) || 
+    (trailing_point_on_polygon.y <= point.y && point.y < a_point_on_polygon.y)
+  end
+
+  def ray_crosses_through_line_segment?(point, a_point_on_polygon, trailing_point_on_polygon)
+    (point.x < (trailing_point_on_polygon.x - a_point_on_polygon.x) * (point.y - a_point_on_polygon.y) / 
+               (trailing_point_on_polygon.y - a_point_on_polygon.y) + a_point_on_polygon.x)
+  end
+
+  def outside_bounding_box?(point)
+    bb_point_1, bb_point_2 = bounding_box
+    max_x = [bb_point_1.x, bb_point_2.x].max
+    max_y = [bb_point_1.y, bb_point_2.y].max
+    min_x = [bb_point_1.x, bb_point_2.x].min
+    min_y = [bb_point_1.y, bb_point_2.y].min
+    
+    point.x < min_x || point.x > max_x || point.y < min_y || point.y > max_y
+  end
+end
+
+class Event
+  attr_accessor :query_id, :location, :range, :time, :duration, :type
+
+  def initialize(id, location, range, time, duration, type)
+    @query_id = id
+    @location = location
+    @range    = range
+    @time     = time
+    @duration = duration
+    @type     = type
+  end
+
+  def to_json(*a)
+    {
+      'json_class'   => self.class.name,
+      'data'         => [ query_id, location, range, time, duration, type ]
+    }.to_json(*a)
+  end
+
+  def self.json_create(o)
+    new(*o['data'])
+  end
+end
+
+# class FireEvent < Event
+#   def initialize(source_starship, destination_starship)
+#
+#   end
+# end
+
+class Starship
+  attr_accessor :captain, :location, :weapons, :armor, :sensors, :hit_points
+
+  def initialize(captain, location, weapons, armor, sensors, hit_points)
+    @captain    = captain
+    @location   = location
+    @weapons    = weapon
+    @armor      = armor
+    @sensors    = sensors
+    @hit_points = hit_points
+  end
+
+  def to_json(*a)
+    {
+      'json_class'   => self.class.name,
+      'data'         => [ captain, location, weapons, armor, sensors, hit_points]
+    }.to_json(*a)
+  end
+
+  def self.json_create(o)
+    new(*o['data'])
+  end
+end
+
+
+# >> e = Event.new(1, [23, 34], 3, 'asdf', 30, "typhoon")
+# => #<Event:0x106e6b8e0 @query_id=1, @time="asdf", @range=3, @type="typhoon", @location=[23, 34], @duration=30>
+# >> json = e.to_json
+# => "{\"data\":[1,[23,34],3,\"asdf\",30,\"typhoon\"],\"json_class\":\"Event\"}"
+# >> e2 = JSON.parse(json)
+# => #<Event:0x106e5cf70 @query_id=1, @time="asdf", @range=3, @type="typhoon", @location=[23, 34], @duration=30>
--- public/js/jug.js
+++ public/js/jug.js
@@ -1,5 +1,50 @@
+function xinspect(o,i){
+    if(typeof i=='undefined')i='';
+    if(i.length>50)return '[MAX ITERATIONS]';
+    var r=[];
+    for(var p in o){
+        var t=typeof o[p];
+        r.push(i+'"'+p+'" ('+t+') => '+(t=='object' ? 'object:'+xinspect(o[p],i+'  ') : o[p]+''));
+    }
+    return r.join(i+'\n');
+}
+
+function GSEvent(json) 
+{ 
+  //id, location, range, time, duration, type
+  switch (typeof arguments[0]) {
+    case 'number' : GSEvent.$args.apply(this, arguments); break;
+    case 'string' : GSEvent.$json.apply(this, arguments); break;
+    default: /*NOP*/
+  } 
+}
+
+GSEvent.$args = function(id, location, range, time, duration, type)
+{
+  this.id = id;
+  this.location = location;
+  this.range = range;
+  this.time = time;
+  this.duration = duration;
+  this.type = type;
+}
+
+GSEvent.$json = function(json)
+{
+  json = JSON.parse(json)
+  if (json.data != null) {
+    this.id = json.data[0];
+    this.location = json.data[1];
+    this.range = json.data[2];
+    this.time = json.data[3];
+    this.duration = json.data[4];
+    this.type = json.data[5];
+  } else {
+    $.extend(this, json);
+  }
+}
+
 var logElement = jQuery("#log");
-logElement.val("hello");
 
 var log = function(data){
   logElement.val( logElement.val() + data + "\n");
@@ -8,23 +53,50 @@ var log = function(data){
   );
 };
 
+var processData = function(json) {
+  obj  = JSON.parse(json)
+  rval = null;
+  switch(obj.json_class)
+  {
+    case "Event":
+      rval = new GSEvent(json)
+      break;
+    default:
+      rval = obj;
+  }
+  return rval;
+}
+
 var jug = new Juggernaut({
   secure: ('https:' == document.location.protocol),
   host: document.location.hostname,
   port: 8080
 });
 
-//jQuery.beforeSend(function(xhr){
-//  xhr.setRequestHeader("X-Session-ID", jug.sessionID);
-//});
+jQuery.ajaxSetup( {
+  'beforeSend': function(xhr){
+    xhr.setRequestHeader("X-Session-ID", jug.sessionID);
+  }
+})
+
+//location = {lat: -32.43535345, longitude: -59.245663}
+jug.meta = {starship_id: 0, client_id: 0};
 
 jug.on("connect", function(){ log("Connected") });
 jug.on("disconnect", function(){ log("Disconnected") });
 jug.on("reconnect", function(){ log("Reconnecting") });
 
-log("Subscribing to channel1");
+log("Subscribing to global");
 
-jug.subscribe("channel1", function(data){
+jug.subscribe("global", function(data){
+  r = processData(data)
+  log("Got json: " + data);
+  log("Got data: " + xinspect(r));
+});
+
+log("Subscribing to zones");
+
+jug.subscribe("zones", function(data){
   log("Got data: " + data);
 });
 
@@ -32,7 +104,8 @@ jug.subscribe("channel1", function(data){
 
 $(document).ready(function() {
   $('#new_event').submit(function() {
-    $.post($(this).attr('action'), $(this).serialize(), function(data){
+    e = new GSEvent(1, [2.3], 4, 5, 6, 7)
+    $.post($(this).attr('action'), e, function(data){
       log("got stuff: " + data);
     }, "text");
     return false;
@@ -42,8 +115,7 @@ $(document).ready(function() {
 $(document).ready(function() {
   $('#new_query').submit(function() {
     $.post($(this).attr('action'), $(this).serialize(), function(data){
-      $('#message').append("<p>" + data + "</p>");
-      $('#new_message').each(function(){this.reset();});
+      log("got stuff: " + data)
     }, "text");
     return false;
   });
